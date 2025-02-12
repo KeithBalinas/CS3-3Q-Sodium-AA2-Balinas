41c075ad3210bacdf8492afef283d2de974b6d36
KeithBalinas <kmbbalinas22@src.pshs.edu.ph>
2025-02-12 15:49:27 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function exponentialPopulationGrowth () {
+    let initialPopulation = prompt('Input the initial population');
+    let rateOfGrowth = prompt('Input the rate of growth');
+    let timeInHours = prompt('Input the time in hours');
+
+    let finalPopulation = parseFloat(initialPopulation) * Math.pow(Math.E,(parseFloat(rateOfGrowth) * parseFloat(timeInHours)));
+
+    let regionOfMonster = prompt('Input the region/location of the monster');
+    let nameOfMonster = prompt('Input the name of the mosnter');
+
+    document.getElementById('result').innerHTML = 'After ' + timeInHours + ' hours, the population of ' + regionOfMonster.concat(' ',nameOfMonster).toUpperCase() + ' has risen to ' + Math.round(finalPopulation);
+}
